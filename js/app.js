2389972388c1aa99b52429784f7953fa428b03c9
Peter Applebee <peterapplebee@gmail.com>
2014-08-19 00:45:58 +0100

    Awesome sequential fadeins

--- js/app.js
+++ js/app.js
@@ -5,27 +5,48 @@
 
 jQuery(document).ready(function() {
 
+
+		
+
 	$('body').on('click', '#start', function(event) {
 		event.preventDefault();
+
+
+
 		for(var x = 1; x <= 100; x++) {
 
 			if ((x % 5 === 0) && (x % 3 === 0)) {
-				$('div#program-output').append('<p class="fizz-buzz">FIZZ-BUZZ</p>');
+				$('div#program-output').append('<p class="num fizz-buzz">FIZZ-BUZZ</p>');
+
 			}
 
 			else if (x % 3 === 0) {
-				$('div#program-output').append('<p class="fizz">FIZZ</p>');
+				$('div#program-output').append('<p class="num fizz">FIZZ</p>');
+				
 			}
 
 			else if (x % 5 === 0) {
-				$('div#program-output').append('<p class="buzz">BUZZ</p>');
+				$('div#program-output').append('<p class="num buzz">BUZZ</p>');
+				
 			}
 
 			else {
-				$('div#program-output').append('<p>'+x+'</p>').hide().fadeIn(200);
+				$('div#program-output').append('<p class="num">'+x+'</p>');
+				
 			};
 
 		};
+
+		var paras = $('p.num'),
+		i = 0;
+		function animateNum () {
+		    $(paras[i++]).fadeIn(100, animateNum)
+		                 .css({opacity:0,"margin-left":"0","display":"inline-block"})
+		                 .animate({opacity:1,"margin-left":"0"});
+		}
+
+		animateNum();
+
 
 	});
 
