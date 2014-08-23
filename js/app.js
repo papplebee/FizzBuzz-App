3fc96be916bd3bf829374761c99a346794c11ce9
Peter Applebee <peterapplebee@gmail.com>
2014-08-23 11:18:02 +0100

    Finished Program working
    Able to detect if NaN's are entered, etc..

--- js/app.js
+++ js/app.js
@@ -11,65 +11,81 @@ jQuery(document).ready(function() {
 		event.preventDefault();
 		$('div#program-output').empty();
 		$(this).closest('#example').find('#start-number').val(1);
-		$(this).closest('#example').find('#finish-number').val(100);
+		$(this).closest('#example').find('#finish-number').val(30);
 
-	})
+		var fizzBuzzCount = function(where) {
+			$(where).html(0);
+		};
+
+		fizzBuzzCount('#fizz-data');
+		fizzBuzzCount('#buzz-data');
+		fizzBuzzCount('#fizzbuzz-data');
+
+	});
 
 	$('body').on('click', '#start', function(event) {
 		event.preventDefault();
 
 		$('div#program-output').empty();
 
-		var startNum = +$(this).closest('#example').find('#start-number').val();
-		var endNum = +$(this).closest('#example').find('#finish-number').val();
+		var startNum = $(this).closest('#example').find('#start-number').val();
+		var endNum = $(this).closest('#example').find('#finish-number').val();
+		console.log(startNum, endNum);
 
-		if ((isNaN(startNum)) || (isNaN(endNum)))  {
-			alert("You have not entered a number. Please enter a number.");
-		}
-		else if ((startNum % 1 != 0) || (endNum % 1 != 0)) {
-			alert("You have not entered a whole number. Please only use whole numbers.")
-		}
-		else if (endNum >= startNum) {
+		if (isNaN(endNum) && isNaN(startNum)) {
+			alert(startNum+" and "+endNum+" are not numbers. Please enter numbers.");
+		} else if (isNaN(endNum)) {
+			alert(endNum+" is not a number. Please enter a number.");
+		} else if (isNaN(startNum))  {			
+			alert(startNum+" is not a number. Please enter a number.");
+		} else {
+			startNum = +startNum;
+			endNum = +endNum;
 
-			for(var x = startNum; x <= endNum; x++) {
-				if ((x % 5 === 0) && (x % 3 === 0)) {
-					$('div#program-output').append('<p class="num fizz-buzz">FIZZBUZZ</p>');
-				}
-				else if (x % 3 === 0) {
-					$('div#program-output').append('<p class="num fizz">FIZZ</p>');				
-				}
-				else if (x % 5 === 0) {
-					$('div#program-output').append('<p class="num buzz">BUZZ</p>');					
-				}
-				else {
-					$('div#program-output').append('<p class="num">'+x+'</p>');					
+			if ((startNum % 1 != 0) || (endNum % 1 != 0)) {			
+				alert("You have not entered a whole number. Please only use whole numbers.")
+			}
+			else if (endNum >= startNum) {
+
+				for(var x = startNum; x <= endNum; x++) {
+					if ((x % 5 === 0) && (x % 3 === 0)) {
+						$('div#program-output').append('<p class="num fizz-buzz">FIZZBUZZ</p>');
+					}
+					else if (x % 3 === 0) {
+						$('div#program-output').append('<p class="num fizz">FIZZ</p>');				
+					}
+					else if (x % 5 === 0) {
+						$('div#program-output').append('<p class="num buzz">BUZZ</p>');					
+					}
+					else {
+						$('div#program-output').append('<p class="num">'+x+'</p>');					
+					};
 				};
+
+				var paragraphs = $('p.num');
+				var i = 0;
+				var animateNum = function () {
+				    $(paragraphs[i++]).fadeIn(100, animateNum)
+				                 .css({opacity:0,"margin-left":"0","display":"inline-block"})
+				                 .animate({opacity:1,});
+				}
+				animateNum();
+
+				var fizzBuzzCount = function(type, where) {
+					var fizzBuzzNum = ($(type).length) - 1;
+					console.log(fizzBuzzNum);
+					$(where).html(fizzBuzzNum);
+				};
+
+				fizzBuzzCount('.fizz', '#fizz-data');
+				fizzBuzzCount('.buzz', '#buzz-data');
+				fizzBuzzCount('.fizz-buzz', '#fizzbuzz-data');
+
+			} 
+			else {
+				alert("Your finish number is less than your start number, please enter a greater number.");
 			};
-
-			var paragraphs = $('p.num');
-			var i = 0;
-			var animateNum = function () {
-			    $(paragraphs[i++]).fadeIn(100, animateNum)
-			                 .css({opacity:0,"margin-left":"0","display":"inline-block"})
-			                 .animate({opacity:1,});
-			}
-			animateNum();
-
-			var fizzBuzzCount = function(type, where) {
-				var fizzBuzzNum = ($(type).length) - 1;
-				console.log(fizzBuzzNum);
-				$(where).html(fizzBuzzNum);
-			}
-
-			fizzBuzzCount('.fizz', '#fizz-data');
-			fizzBuzzCount('.buzz', '#buzz-data');
-			fizzBuzzCount('.fizz-buzz', '#fizzbuzz-data');
-
-		} 
-		else {
-			alert("Your finish number is less than your start number, please enter a greater number.");
 		};
-
 
 	});
 
